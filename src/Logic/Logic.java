e60ae64d14037de99f6e7c0fb26777fc432eada8
NIGEL <devnull@localhost>
2014-03-09 23:16:52 +0800

    changed time type to calendar type. waiting for narin to implement calendar to storage.

--- src/Logic/Logic.java
+++ src/Logic/Logic.java
@@ -1,224 +1,191 @@
 package Logic;
 
 import Storage.*;
+
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Calendar;
 
-//since we use allTasks in all the commands  is it better to use a global variable?
-
 public class Logic {
-	
+
 	private static final long CONSTANT_MILLISECONDS_IN_A_DAY = 86400000;
-	
+
 	private static String MESSAGE_TASK_NOT_DELETED = "The task indicated cannot be deleted as it does not exist.";
 	private static String MESSAGE_TASK_INDEX_INVALID = "The given task number is invalid.";
-	
+
 	private static String MESSAGE_TASK_DELETED = "The Task has been successfully deleted.";
 	private static String MESSAGE_TASK_IS_DONE = "has been completed";
-	
-	public static void addTask(AllTasks allTasks, Time startTime, Time endTime, boolean isThereReminder, boolean isTaskDone, boolean isAllDayEvent, 
-			String taskTitle, String remarks, Time reminderTime) {
-		int counter = 0, dummyTaskID=0;
-		boolean check = true;
-		
-		Task task = new Task(dummyTaskID, startTime, endTime, isThereReminder, isTaskDone, isAllDayEvent, taskTitle, remarks);
-		
-		while (counter < allTasks.getSize() && check) {
-			if (allTasks.getTask(counter).getStartTime().getYear() >= startTime.getYear()) {
-				counter= counter - 1;
-				check = false;
-			}
-			counter = counter + 1;
-		}
-		
-		check = true;
-		while (counter < allTasks.getSize() && check) {
-			if (allTasks.getTask(counter).getStartTime().getMonth() >= startTime.getMonth()) {
-				counter= counter - 1;
-				check = false;
-			}
-			counter = counter + 1;
-		}
-		
-		check = true;
-		while (counter < allTasks.getSize() && check) {
-			if (allTasks.getTask(counter).getStartTime().getDay() >= startTime.getDay()) {
-				counter= counter - 1;
-				check = false;
-			}
-			counter = counter + 1;
-		}
-		
-		check = true;
-		while (counter < allTasks.getSize() && check) {
-			if (allTasks.getTask(counter).getStartTime().getHour() >= startTime.getHour()) {
-				counter= counter - 1;
-				check = false;
-			}
-			counter = counter + 1;
-		}
-		
-		check = true;
-		while (counter < allTasks.getSize() && check) {
-			if (allTasks.getTask(counter).getStartTime().getMinute() >= startTime.getMinute()) {
-				counter= counter - 1;
-				check = false;
-			}
-			counter = counter + 1;
-		}
-		
-		allTasks.addTask(counter, task);
-		addReminder(allTasks, reminderTime, task);
-		return;
 
-	}
-	
-	private static void addReminder (AllTasks allTasks, Time reminderTime, Task task) {
-		int counter = 0;
-		boolean check = true;
-		
-		Reminder reminder = new Reminder (reminderTime, task);
-		
-		while (counter < allTasks.getReminderSize() && check) {
-			if (allTasks.getReminder(counter).getReminderTime().getYear() >= reminderTime.getYear()) {
-				counter= counter - 1;
-				check = false;
-			}
-			counter = counter + 1;
-		}
-		
-		check = true;
-		while (counter < allTasks.getSize() && check) {
-			if (allTasks.getReminder(counter).getReminderTime().getMonth() >= reminderTime.getMonth()) {
-				counter= counter - 1;
-				check = false;
-			}
-			counter = counter + 1;
-		}
-		
-		check = true;
-		while (counter < allTasks.getSize() && check) {
-			if (allTasks.getReminder(counter).getReminderTime().getDay() >= reminderTime.getDay()) {
-				counter= counter - 1;
-				check = false;
-			}
-			counter = counter + 1;
-		}
-		
-		check = true;
-		while (counter < allTasks.getSize() && check) {
-			if (allTasks.getReminder(counter).getReminderTime().getHour() >= reminderTime.getHour()) {
-				counter= counter - 1;
-				check = false;
-			}
-			counter = counter + 1;
-		}
-		
-		check = true;
-		while (counter < allTasks.getSize() && check) {
-			if (allTasks.getReminder(counter).getReminderTime().getMinute() >= reminderTime.getMinute()) {
-				counter= counter - 1;
-				check = false;
-			}
-			counter = counter + 1;
-		}
-		
-		allTasks.addReminder(counter, reminder);
-		return;
+	private static AllTasks _storage;
 
+	public static void initStorage(AllTasks alltasks) {
+		AllTasks _storage = alltasks;
 	}
-	
-	public static void deleteTask (AllTasks allTasks, int index) {
-		if (index >= allTasks.getSize()){
-			System.out.printf("%s/n", MESSAGE_TASK_NOT_DELETED);
-		} else{
-			if (allTasks.getTask(index).getIsThereReminder()) {
-				allTasks.deleteReminder(allTasks.getTask(index));
+
+	public static void addTask(Date start, Date end, boolean isThereReminder,
+			boolean isTaskDone, boolean isAllDayEvent, String title,
+			String remarks, Date reminder) {
+
+		Calendar taskStart = Calendar.getInstance();
+		taskStart.setTime(start);
+
+		Calendar taskEnd = Calendar.getInstance();
+		taskEnd.setTime(end);
+
+		Date current = new Date();
+
+		boolean isValidDuration = start.before(end);
+		boolean isEventNotEnded = current.before(end);
+
+		if (isValidDuration && isEventNotEnded) {
+			int TaskId = 0;
+			int totalNumOfTasks = _storage.getSize();
+			int count = 0;
+			// taskStart will have a larger time then the i task, incremental i
+			// task till taskStart smaller.
+			boolean IsTaskNotInCorrectPosition = taskStart.compareTo(_storage
+					.getTask(count).startTime()) > 0;
+			while ((count < totalNumOfTasks) && (IsTaskNotInCorrectPosition)) {
+				count++;
+				IsTaskNotInCorrectPosition = taskStart.compareTo(_storage
+						.getTask(count).startTime()) > 0;
 			}
-			allTasks.deleteTask(index);
-			System.out.printf("%s/n", MESSAGE_TASK_DELETED);
-			
+			if (count != 0) { // correct position
+				count--;
+			}
+
+			Task task = new Task(TaskId, taskStart, taskEnd, isThereReminder,
+					isTaskDone, isAllDayEvent, title, remarks);
+
+			_storage.addTask(count, task);
+
+			Calendar reminderTime = Calendar.getInstance();
+			reminderTime.setTime(reminder);
+
+			Reminder taskReminder = new Reminder(reminderTime, task);
+
+			count = 0;
+
+			boolean IsReminderNotInCorrectPosition = reminderTime
+					.compareTo(_storage.getReminder(count).getReminderTime()) > 0;
+			int totalNumOfReminders = _storage.getReminderSize();
+
+			while ((count < totalNumOfReminders)
+					&& (IsReminderNotInCorrectPosition)) {
+				count++;
+				IsReminderNotInCorrectPosition = reminderTime
+						.compareTo(_storage.getReminder(count)
+								.getReminderTime()) > 0;
+			}
+			if (count != 0) { // correct position
+				count--;
+			}
+
+			_storage.addReminder(count, taskReminder);
 		}
-		return;
 	}
-	
-	public static void taskIsDone (AllTasks allTasks, int index)  {
-		if(allTasks.getSize() <= index) {
-			System.out.printf("%s/n", MESSAGE_TASK_INDEX_INVALID);
-		} else{
-			allTasks.getTask(index).setIsTaskDone(true);
-			System.out.printf("Task Number %d %s/n",index , MESSAGE_TASK_IS_DONE);
-		}
-		return;
-	}
-	
-	public static void displayTask(AllTasks allTasks, int index) {
-		
-		int totalNumOfTasks = allTasks.getSize();
-		
+
+	public static void deleteTask(int index) {
+		int totalNumOfTasks = _storage.getSize();
 		if (index < totalNumOfTasks) {
-		Task task = allTasks.getTask(index);
-		int indexOfReminder = allTasks.searchForCorrespondingReminder(task);
-		
-			System.out.printf("Task Number: %d Start Time: %d:%d %d/%d/%d End Time: %d:%d %d/%d/%d\n"
-					+ "Name: %s\nTask is done: %b\nTask is all day event: %b\n"
-					+ "Remarks: %s\nReminder Time: %d:%d %d/%d/%d\n", allTasks.getTask(index).getTaskID(), allTasks.getTask(index).getStartTime().getHour(),
-					allTasks.getTask(index).getStartTime().getMinute(), allTasks.getTask(index).getStartTime().getDay(), allTasks.getTask(index).getStartTime().getMonth(),
-					allTasks.getTask(index).getStartTime().getYear(),allTasks.getTask(index).getEndTime().getHour(), allTasks.getTask(index).getEndTime().getMinute(),
-					allTasks.getTask(index).getEndTime().getDay(), allTasks.getTask(index).getEndTime().getMonth(), allTasks.getTask(index).getEndTime().getYear(),
-					allTasks.getTask(index).getTaskTitle(),allTasks.getTask(index).getIsTaskDone(), allTasks.getTask(index).getIsAllDayEvent(),
-					allTasks.getTask(index).getRemarks(), allTasks.getReminder(indexOfReminder).getReminderTime().getHour(), allTasks.getReminder(indexOfReminder).getReminderTime().getMinute(),
-					allTasks.getReminder(indexOfReminder).getReminderTime().getDay(), allTasks.getReminder(indexOfReminder).getReminderTime().getMonth(),
-					allTasks.getReminder(indexOfReminder).getReminderTime().getYear());
-			
+
+			if (_storage.getTask(index).getIsThereReminder()) {
+				_storage.deleteReminder(_storage.getTask(index));
+			}
+			_storage.deleteTask(index);
+			System.out.printf("%s/n", MESSAGE_TASK_DELETED);
+
+		} else {
+			System.out.printf("%s/n", MESSAGE_TASK_NOT_DELETED);
 		}
-		
-	else {
-		System.out.printf("%s/n", MESSAGE_TASK_INDEX_INVALID);
 	}
+
+	public static void taskIsDone(int index) {
+		int totalNumOfTasks = _storage.getSize();
+		if (index < totalNumOfTasks) {
+			_storage.getTask(index).setIsTaskDone(true);
+			System.out.printf("Task Number %d %s/n", index,
+					MESSAGE_TASK_IS_DONE);
+		} else {
+			System.out.printf("%s/n", MESSAGE_TASK_INDEX_INVALID);
+		}
+		return;
 	}
-	
-	
-	public static void displayAllTasks(AllTasks allTasks) {
-		int totalNumOfTasks = allTasks.getSize();
+
+	public static void displayTask(int index) {
+
+		int totalNumOfTasks = _storage.getSize();
+
+		if (index < totalNumOfTasks) {
+			Task task = _storage.getTask(index);
+			int indexOfReminder = _storage.searchForCorrespondingReminder(task);
+
+			Date startTime = task.getStartTime().getTime();
+			Date endTime = task.getEndTime().getTime();
+
+			SimpleDateFormat display = new SimpleDateFormat(
+					"E yyyy.MM.dd 'at' hh:mm:ss a zzz");
+			System.out.println("id: " + task.getTaskID());
+			System.out.println("start: "
+					+ display.format(task.getStartTime().getTime()));
+			System.out.println("end: "
+					+ display.format(task.getEndTime().getTime()));
+			System.out.println("title: " + task.getTaskTitle());
+			System.out.println("remarks: " + task.getRemarks());
+			System.out.println("reminder: "
+					+ display.format(task.getReminder(indexOfReminder)
+							.getReminderTime().getTime()));
+			System.out.println((task.getIsTaskDone()) ? "YES" : "NO");
+			System.out.println((task.getIsAllDayEvent()) ? "YES" : "NO");
+		}
+
+		else {
+			System.out.printf("%s/n", MESSAGE_TASK_INDEX_INVALID);
+		}
+	}
+
+	public static void displayAllTasks() {
+		int totalNumOfTasks = _storage.getSize();
 		for (int count = 0; count < totalNumOfTasks; count++) {
-			displayTask(allTasks, count);
+			displayTask(_storage, count);
 		}
 	}
-	
-	public static void displayTasksAtPeriod(AllTasks allTasks, Date startDateSpecified, Date endDateSpecified) {
-		int totalNumOfTasks = allTasks.getSize();
-		
+
+	public static void displayTasksAtPeriod(Date startDateSpecified,
+			Date endDateSpecified) {
+		int totalNumOfTasks = _storage.getSize();
+
 		Date startTime, endTime;
 		for (int count = 0; count < totalNumOfTasks; count++) {
-			Task task = allTasks.getTask(count);
-			
-			//convert 'Calendar' to 'Date' to compare
+			Task task = _storage.getTask(count);
+
+			// convert 'Calendar' to 'Date' to compare
 			startTime = task.getStartTime.getTime();
 			endTime = task.getEndTime.getTime();
-			
+
 			boolean hasTaskStarted = startTime.after(startDateSpecified);
 			boolean hasTaskNotEnded = endTime.before(endDateSpecified);
-			
+
 			if (hasTaskStarted && hasTaskNotEnded) {
-			displayTask(allTasks, count);
+				displayTask(count);
 			}
-		}	
-	}
-	
-	public static void displayTasksAtDate(AllTasks allTasks, Date dateSpecified) {
-	displayTasksAtPeriod(allTasks, dateSpecified, dateSpecified);
-	}
-	
-	public static void displayToday (AllTasks allTasks) {
-	Date today = new Date();
-	displayTasksAtDate(allTasks, today);
+		}
 	}
 
-	public static void displayTomorrow (AllTasks allTasks) {
-    long tomorrowInMillis = System.currentTimeMillis() + CONSTANT_MILLISECONDS_IN_A_DAY;
-	Date tomorrow = new Date(tomorrowInMillis);
-	displayTasksAtDate(allTasks, tomorrow);
-	}	
-	
+	public static void displayTasksAtDate(Date dateSpecified) {
+		displayTasksAtPeriod(dateSpecified, dateSpecified);
+	}
+
+	public static void displayToday(AllTasks allTasks) {
+		Date today = new Date();
+		displayTasksAtDate(today);
+	}
+
+	public static void displayTomorrow(AllTasks allTasks) {
+		long tomorrowInMillis = System.currentTimeMillis()
+				+ CONSTANT_MILLISECONDS_IN_A_DAY;
+		Date tomorrow = new Date(tomorrowInMillis);
+		displayTasksAtDate(tomorrow);
+	}
+
 }
