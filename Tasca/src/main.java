e09ede5d47d0e9a5f938a10c557ee6fad7231c0c
Narinderpal Singh Dhillon <devnull@localhost>
2014-03-12 14:35:05 +0800

    added Welcome Message

--- Tasca/src/main.java
+++ Tasca/src/main.java
@@ -6,7 +6,9 @@ public class main {
 		boolean quit = false ;
 		Controller controller = new Controller();
 		
+		String MESSAGE_WELCOME_V0_1 =  "Welcome to Tasca V0.1!\n";
 		
+		System.out.printf("%s", MESSAGE_WELCOME_V0_1);
 		while (!quit) {
 			quit = controller.executeCommands();
 		}
