6b9edfbf82ea0b77808eac71b6b6cc0c41ebc50c
NIGEL <devnull@localhost>
2014-03-25 14:24:55 +0800

    Fixed simulator.java for command line input

---
+++ Tasca/src/storage/AllTasksTest2.java
@@ -0,0 +1,17 @@
+package storage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AllTasksTest2 {
+	private AllTasks test;
+	
+	@Test
+	public void test() {
+test = new AllTasks();
+//where is the save command. where what is the save file.
+		fail("Not yet implemented");
+	}
+
+}
